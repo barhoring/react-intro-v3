95d5eec1ee34bca7bb05155e235d67ac0e795497
Bar Horing <bar.apple@gmail.com>
2018-08-04 23:46:11 +0300

    cool. a big picture with an text input

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const MyTitle = function(props) {
- return(
-   <div>
-     <h1 style={ {color: props.color } }>{props.title}</h1>
-   </div>
- );
-};
-
-const MyFirstComponent = function() {
-  return <div id="my-first-component">
-          <MyTitle title='Game of Pot3' color='YellowGreen' />
-          <MyTitle title='Stranger Things' color='peru' />
-          <MyTitle title='Silicon Valley' color='GreenYellow' />
-          <MyTitle title='Rick and Puddy' color='burlywood' />
+const App = () =>
+    <div className='app'>
+      <div className='landing'>
+        <h1>svideo</h1>
+        <input type='text' placeholder='Search' />
+        <a href='google.com' >Or browse all</a>
+      </div>
     </div>
-};
 
-render(<MyFirstComponent />, document.getElementById('app'));
+render(<App />, document.getElementById('app'))
