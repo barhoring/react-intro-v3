04c5ef05f54aa01d3c33d30980b8e4a951184207
Bar Horing <bar.apple@gmail.com>
2018-08-04 22:07:47 +0300

    saving

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
-const ce = React.createElement;
 
 const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+ return(
+   <div>
+     <h1 style={ {color: props.color } }>{props.title}</h1>
+   </div>
+ );
 };
 
 const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Pot3', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
-  );
+  return <div id="my-first-component">
+          <MyTitle title='Game of Pot3' color='YellowGreen' />
+          <MyTitle title='Stranger Things' color='peru' />
+          <MyTitle title='Silicon Valley' color='GreenYellow' />
+          <MyTitle title='Rick and Puddy' color='burlywood' />
+    </div>
 };
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(<MyFirstComponent />, document.getElementById('app'));
