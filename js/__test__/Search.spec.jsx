3387727ea2465b6a6f2196cb8557dfae5cb8d3ef
Bar Horing <bar.apple@gmail.com>
2018-08-22 21:30:20 +0300

    next: testing the search field

--- js/__test__/Search.spec.jsx
+++ js/__test__/Search.spec.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Search from '../Search'
+import preload from '../../data.json';
+import Search from '../Search';
+import ShowCard from '../ShowCard';
 
 // console.log(process.env.NODE_ENV); // who knew? jest sets a NODE_ENV variable
 
 test('Search renders correctly', () => {
     const component = shallow(<Search />);
     expect(component).toMatchSnapshot();
+});
+
+test('Search should render correct amount of shows', () => {
+    const component = shallow(<Search />);
+    expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+});
+
+xtest('Search should render correct amount of shows', () => {
+
 });
