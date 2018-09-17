32be8a7f2343c9d9197f90f4f4483ec7ee2c1f48
Bar Horing <bar.apple@gmail.com>
2018-09-17 22:12:22 +0300

    creating search and details bundle

--- js/App.jsx
+++ js/App.jsx
@@ -6,8 +6,6 @@ import type { Match } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import AsyncRoute from './AsyncRoute';
-import Search from './Search';
-import Details from './Details';
 import preload from '../data.json';
 
 const FourOhFour = () => <h1>404</h1>;
@@ -19,10 +17,14 @@ const App = () => (
           <Route exact path="/" component={ props => 
             <AsyncRoute props={props} loadingPromise={import('./Landing')} />
           } />
-          <Route path="/search" component={props => <Search shows={preload.shows} {...props} />} />
+          <Route path="/search" component={props => 
+            <AsyncRoute props={Object.assign({ shows: preload.shows }, props)} loadingPromise={import('./Search')} />
+          } />
           <Route path="/details/:id" component={(props: { match: Match }) => {
-            const tmp = preload.shows.find(show=> show.imdbID === props.match.params.id);
-            return <Details show={tmp} {...props} />
+            const selectedShow = preload.shows.find(show=> show.imdbID === props.match.params.id);
+            return (
+              <AsyncRoute props={Object.assign({show: selectedShow, match: {} }, props)} loadingPromise={import('./Details')} />
+            );
           }} />
           <Route component={FourOhFour} />
         </Switch>
